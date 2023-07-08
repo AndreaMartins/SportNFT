35158598412fa1b06bf2843e5cbdc73eaf723dd7
dagonib <davidgonzalezibanez@gmail.com>
2023-07-08 16:46:41 +0200

    2.Backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 
-// Inicio
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  // Inicio Development status.
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Screens
+import HomeScreen from './screens/HomeScreen';
+
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
-    <>
-      <h1>Sport NFT</h1>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} exact />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   );
 };
 export default App;
